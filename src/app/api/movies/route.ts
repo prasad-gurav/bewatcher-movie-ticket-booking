a93a4cd2f613f7a48191374814d2f4469726708b
Prasad Gurav <psgurav2001@gmail.com>
2024-09-05 18:11:39 +0530

    BUGFIX -  date generate issue at runtime.

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -1,21 +1,31 @@
 import React from 'react'
 import { NextRequest, NextResponse } from 'next/server'
-import { todayFormatted,fifteenDaysAgoFormatted } from '@/utils/getDate'
+
+
+const formatDate = (date:Date) => {
+    return date.toISOString().split('T')[0];
+  };
 
 
 export async function GET(request: NextRequest) {
-    console.log(todayFormatted)
+    const todayDate = new Date();
+    const fifteenDaysAgoDate = new Date(todayDate);
+    fifteenDaysAgoDate.setDate(fifteenDaysAgoDate.getDate() - 30);
+    
+    const today = formatDate(todayDate);
+    const fifteenDaysAgo = formatDate(fifteenDaysAgoDate);
+
     if (request.method === 'GET') {
 
         let headersList = {
             "accept": "application/json",
             "Authorization": "Bearer "
         }
-        const movie_db_url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=hi-IN&page=1&sort_by=popularity.desc&with_release_type=2%7C3&release_date.gte=${fifteenDaysAgoFormatted}&release_date.lte=${todayFormatted}&api_key=${process.env.MOVIE_DB_API_KEY!}&with_origin_country=IN`
-
+        const movie_db_url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=hi-IN&page=1&sort_by=popularity.desc&with_release_type=2%7C3&release_date.gte=${fifteenDaysAgo}&release_date.lte=${today}&api_key=${process.env.MOVIE_DB_API_KEY!}&with_origin_country=IN`
+        console.log(movie_db_url)
         let response = await fetch(movie_db_url, {
         method: "GET",
-        // headers: headersList
+        headers: headersList
         });
         if (response.ok){
             const jsonData = await response.json();
