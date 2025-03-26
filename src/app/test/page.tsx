1258f85aac2c6d395c4a617293654470861db134
Prasad Gurav <psgurav2001@gmail.com>
2025-03-26 21:38:55 +0530

    Fixed SSR issue in audi page.

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import CinemaLottie from "@/components/CinemaLottie";
-import React from 'react'
-
-export default function page() {
-  return (
-    <div><CinemaLottie/></div>
-  )
-}
--- src/utils/GenerateQRCode.js
+++ src/utils/GenerateQRCode.js
@@ -1,15 +1,15 @@
-// GenerateQRCode.js
-
-const QRCode = require("qrcode");
+import QRCode from "qrcode";
 
 export async function generateQRCode(data) {
-  try {
-    const qrCodeDataUrl = await QRCode.toDataURL(data);
-    return qrCodeDataUrl;
-  } catch (error) {
-    console.error("Error generating QR code:", error);
-    throw error;
-  }
+	if (typeof window === "undefined") {
+		console.error("generateQRCode can only be used on the client-side.");
+		return null;
+	}
+
+	try {
+		return await QRCode.toDataURL(data);
+	} catch (error) {
+		console.error("Error generating QR code:", error);
+		return null;
+	}
 }
-
-
